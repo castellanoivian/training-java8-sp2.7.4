dcb6d322c8aa0129d6cfdaf159748f1f1146fd21
Lisandra <ltespinosa2020@gmail.com>
2022-09-04 16:07:35 +0200

    Creación del esqueleto, con clase inicial

---
+++ service-score/src/main/java/com/score/ScoreApplication.java
@@ -0,0 +1,11 @@
+package com.score;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+@SpringBootApplication
+public class ScoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ScoreApplication.class, args);
+        System.out.println("*--- SERVICE QUERY SERVER IS UP AND RUNNING ---*");
+    }
+}
