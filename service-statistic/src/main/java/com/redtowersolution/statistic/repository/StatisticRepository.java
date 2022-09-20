6c1c19b1aede9f29ee01ac122a6b068f43c25805
Ivian Castellano <iviancastellano@gmail.com>
2022-09-20 23:08:49 +0200

    Implementando el metodo saveScoreStatistic

--- service-statistic/src/main/java/com/redtowersolution/statistic/repository/StatisticRepository.java
+++ service-statistic/src/main/java/com/redtowersolution/statistic/repository/StatisticRepository.java
@@ -6,7 +6,11 @@ import com.redtowersolution.statistic.model.Statistic;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface StatisticRepository extends JpaRepository<Statistic, Integer> {
+    @Override
+    <S extends Statistic> List<S> saveAll(Iterable<S> listStatistic);
 }
