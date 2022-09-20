1f7237053b13611d9283dabcbbb8217115a94a6d
castellanoivian <castellanoivian@gmail.com>
2022-09-20 21:06:27 +0200

    Funcionalidad que expone la api que retorna una lista de scores de determiando ID cliente.

--- service-query/src/main/java/com/redtowersolution/query/controller/ApiQuery.java
+++ service-query/src/main/java/com/redtowersolution/query/controller/ApiQuery.java
@@ -1,10 +1,27 @@
 package com.redtowersolution.query.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class ApiQuery {
+
+    @GetMapping("/scoreByIdCliente")
+    public List<Object> getScoresByIdCliente(@RequestParam int clienteID){
+        String urlScore = "https://localhost:8080/scoresByIdCliente/"+clienteID;
+        RestTemplate restTemplate = new RestTemplate();
+        Object[] scoreArray = restTemplate.getForObject(urlScore, Object[].class);
+        if (scoreArray != null)
+            return Arrays.asList(scoreArray);
+        else
+            return null;
+    }
 
 }
