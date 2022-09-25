f54f1f61e28395e32748e6dcf9462ff5d487e3b6
castellanoivian <castellanoivian@gmail.com>
2022-09-25 21:03:09 +0200

    Cambios en pom -excluir jpa

--- service-query/src/main/java/com/redtowersolution/query/QueryApplication.java
+++ service-query/src/main/java/com/redtowersolution/query/QueryApplication.java
@@ -2,8 +2,9 @@ package com.redtowersolution.query;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class QueryApplication {
     public static void main(String[] args) {
         SpringApplication.run(QueryApplication.class, args);
