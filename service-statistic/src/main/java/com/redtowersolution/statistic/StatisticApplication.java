6f0cb891b7a44415f482272a2271245e8ed82a27
xavireds89 <javier.rg.garcia@ctw.bmwgroup.com>
2022-09-25 20:28:05 +0100

    Resolve run service-score,service-statistic and service-query

--- service-statistic/src/main/java/com/redtowersolution/statistic/StatisticApplication.java
+++ service-statistic/src/main/java/com/redtowersolution/statistic/StatisticApplication.java
@@ -1,14 +1,31 @@
 package com.redtowersolution.statistic;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 
 @SpringBootApplication
-
+@EntityScan("com.redtowersolution.common.model")
 public class StatisticApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(StatisticApplication.class, args);
-        System.out.println("*--- SERVICE QUERY SERVER IS UP AND RUNNING ---*");
+    private static final Logger LOG = LoggerFactory.getLogger(StatisticApplication.class);
+
+    public static void main(String[] args) throws UnknownHostException {
+        SpringApplication app = new SpringApplication(StatisticApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running! Access URLs:\n\t" +
+                        "Local: \t\thttp://127.0.0.1:{}\n\t" +
+                        "External: \thttp://{}:{}\n----------------------------------------------------------",
+                env.getProperty("spring.application.name", "service-statistic"),
+                env.getProperty("server.port"),
+                InetAddress.getLocalHost().getHostName(),
+                env.getProperty("server.port"));
     }
 }
