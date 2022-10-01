0890dc2ccc635a7a03bafdf4e6242c18d060601b
castellanoivian <castellanoivian@gmail.com>
2022-10-01 21:21:19 +0200

    UPD funcionalidad que comunica server Score con Statistic mediante listado de ids cliente

--- service-query/src/main/java/com/redtowersolution/query/controller/ApiQuery.java
+++ service-query/src/main/java/com/redtowersolution/query/controller/ApiQuery.java
@@ -8,12 +8,13 @@ import org.springframework.web.client.RestTemplate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
 public class ApiQuery {
 
-    @GetMapping("/scoreByIdCliente")
+    /*@GetMapping("/scoreByIdCliente")
     public List<Statistic> getScoresByIdCliente(@RequestParam("clienteID") int clienteID){
         RestTemplate restTemplate = new RestTemplate();
         String urlScore = "http://localhost:8080/viewScoreIdCliente/{clienteID}";
@@ -24,6 +25,20 @@ public class ApiQuery {
         String urlStatistic = "http://localhost:5658/saveScore";
 
         return restTemplate.postForObject(urlStatistic, listStatistic, List.class) ;
+    }*/
+
+    @GetMapping("/scoreByIdClientes")
+    public List<Statistic> scoresByIdClientes(@RequestParam("clienteID") List<Integer> listClienteID){
+        String listOfIds = listClienteID.stream().map(Object::toString).collect(Collectors.joining(","));
+        RestTemplate restTemplate = new RestTemplate();
+        String urlScore = "http://localhost:7890/viewScoreIdClientes/{listClienteID}";
+        Score[] scoreArray = restTemplate.getForObject(urlScore, Score[].class,listOfIds);
+        List<Score> listScore = Arrays.asList(scoreArray);
+        List<Statistic> listStatistic = new ArrayList<>();
+        listScore.forEach((n) -> listStatistic.add(new Statistic(n.getClienteID(),n.getScore())));
+        String urlStatistic = "http://localhost:5658/saveScore";
+
+        return restTemplate.postForObject(urlStatistic, listStatistic, List.class);
     }
 
 }
